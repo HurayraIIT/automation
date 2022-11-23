94230b4a617fbd145b7fac9f86e50eab3b29beb4
Abu Hurayra <hurayraiit@gmail.com>
2022-11-23 10:36:04 +0600

    completed chapter 1

--- playwright.config.js
+++ playwright.config.js
@@ -42,6 +42,8 @@ const config = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    browserName: 'chromium',
+    headless: false
   },
 
   /* Configure projects for major browsers */
--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -1,5 +1,17 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
-test('First Playwright Test', async function () {
-    // Write Code Here
+// test('Browser Context Playwright Test', async ({browser}) => {
+//     // Write Code Here
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
+//     await page.goto("https://betterdocs.co/docs");
+//     console.log(await page.title());
+//     await expect(page).toHaveTitle("Documentation - BetterDocs");
+//     await page.close();
+// });
+
+test('SMEVai Login Test', async ({ page }) => {
+    await page.goto("https://app.smevai.com");
+    await expect(page).toHaveTitle("SME Vai | Login");
+    await page.close();
 });
