644219f9d2f574cc54e7a7b4d7dcb4135e53c378
Abu Hurayra <hurayraiit@gmail.com>
2023-08-16 13:08:27 +0600

    added - EB Pricing Table Test

--- playwright.config.js
+++ playwright.config.js
@@ -2,31 +2,21 @@
 import { defineConfig, devices } from "@playwright/test";
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   timeout: 100000,
-  workers: 6,
-  retries: 2,
+  workers: 10,
+  retries: 3,
 
   testDir: "./tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  // retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
@@ -34,7 +24,6 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
 
-    // An object containing additional HTTP headers to be sent with every request.
     extraHTTPHeaders: {
       'X-QA-TEST': 'Abu Hurayra',
     },
